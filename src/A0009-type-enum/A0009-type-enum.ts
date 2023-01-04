f28c38874e05d08d6102b96e607483c85f961fab
Ivan <ivan.reisrosa@gmail.com>
2023-01-04 09:26:27 -0300

    type enum

---
+++ src/A0009-type-enum/A0009-type-enum.ts
@@ -0,0 +1,22 @@
+enum Colors {
+  RED = 10, // 10
+  BLUE = 100, // 100
+  YELLOW = 200, // 200
+}
+
+enum Colors {
+  PURPLE = 'ROXO',
+  GREEN = 201,
+  PINK,
+}
+
+// console.log(Colors);
+// console.log(Colors.RED);
+// console.log(Colors[10]);
+// console.log(Colors.PURPLE);
+
+export function chooseColor(color: Colors): void {
+  console.log(Colors[color]);
+}
+
+chooseColor(Colors.PURPLE);
