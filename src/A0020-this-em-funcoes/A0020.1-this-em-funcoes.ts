99cfac2914afb30bb262269b41b49c1e70b7688e
Ivan <ivan.reisrosa@gmail.com>
2023-01-04 23:09:27 -0300

    this em funcoes

---
+++ src/A0020-this-em-funcoes/A0020.1-this-em-funcoes.ts
@@ -0,0 +1,7 @@
+export function funcao(this: Date, name: string, age: number): void {
+  console.log(this);
+  console.log(name, age);
+}
+
+funcao.call(new Date(), 'Ivan', 28);
+funcao.apply(new Date(), ['Ivan', 28]);
